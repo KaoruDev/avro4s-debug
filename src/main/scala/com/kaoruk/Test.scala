4848f940bcb61381039557491fa41728fa49da5e
Kaoru Kohashigawa <kaoru@meettally.com>
2019-04-11 09:45:48 -0700

    simplify things

--- src/main/scala/com/kaoruk/Test.scala
+++ src/main/scala/com/kaoruk/Test.scala
@@ -7,13 +7,13 @@ import com.sksamuel.avro4s.{AvroInputStream, AvroOutputStream, AvroSchema}
 
 sealed trait Ingredient
 
-//case class Artichoke(vegan: Boolean) extends Ingredient
-case class Mushroom(vegan: Boolean) extends Ingredient
-case class Peperoni(vegan: Boolean, organic: Boolean) extends Ingredient
+case class Artichoke(foobar: Boolean, count: Int) extends Ingredient
+case class Mushroom(vegan: Boolean, count: Int) extends Ingredient
+case class Peperoni(vegan: Boolean, organic: String) extends Ingredient
 
 sealed trait CrustType
 
-//case object HandTossed extends CrustType
+case object HandTossed extends CrustType
 case object Pan extends CrustType
 case object Thin extends CrustType
 
@@ -21,10 +21,11 @@ case object Thin extends CrustType
 import scala.util.{Failure, Success, Try}
 
 object Settings {
-  val VERSION = 1
+  val VERSION = 2
 
-  val SCHEMA_PATH = s"avro-tmp/schemas/pizza-schema-$VERSION.avcs"
-  val MESSAGE_PATH = s"avro-tmp/messages/pizza-$VERSION"
+  val SCHEMA_PATH = s"avro-out/schemas/pizza-schema-$VERSION.avcs"
+  val BASE_MESSAGE_PATH = "avro-out/messages/pizza"
+  val MESSAGE_PATH = s"$BASE_MESSAGE_PATH-$VERSION"
 }
 
 case class Pizza(name: String, ingredient: Seq[Ingredient], crustType: CrustType)
@@ -33,16 +34,20 @@ class Runner {
   val schema = AvroSchema[Pizza]
 
   def write(): Unit ={
-    val pizza = Pizza("Pep", Seq(Peperoni(vegan = false, organic = false)), Pan)
-
     val os = AvroOutputStream.binary[Pizza].to(new File(Settings.MESSAGE_PATH)).build(schema)
-    os.write(pizza)
+
+//    val pepPizza = Pizza("Pep", Seq(Peperoni(vegan = false, organic = "No Anitbiotics")), Pan)
+//    os.write(pepPizza)
+
+    val mushPizza = Pizza("Mush", Seq(Mushroom(vegan = true, count = 5)), Pan)
+    os.write(mushPizza)
+
     os.flush()
     os.close()
   }
 
-  def read(): Unit = {
-    val is = AvroInputStream.binary[Pizza].from(new File(Settings.MESSAGE_PATH)).build(schema)
+  def read(version: Int): Unit = {
+    val is = AvroInputStream.binary[Pizza].from(new File(s"${Settings.BASE_MESSAGE_PATH}-$version")).build(schema)
     val pizzas = is.iterator.toSet
     is.close()
 
@@ -64,7 +69,7 @@ class Runner {
 object Test extends App {
   val runner = new Runner()
 
-  runner.writeSchema()
-  runner.write()
-//  runner.read()
+//  runner.writeSchema()
+//  runner.write()
+  runner.read(version = 1)
 }
